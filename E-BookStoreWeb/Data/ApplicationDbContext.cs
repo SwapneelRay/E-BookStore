f5382ff3e8c3901fa8621e77a0a9bfbcb2587b3a
SwapneelRay <46602995+SwapneelRay@users.noreply.github.com>
2024-03-29 01:04:52 +0530

    SSMS 2022 connection String configured and Categories table created.

---
+++ E-BookStoreWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using E_BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_BookStoreWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- E-BookStoreWeb/Program.cs
+++ E-BookStoreWeb/Program.cs
@@ -1,3 +1,6 @@
+using E_BookStoreWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace E_BookStoreWeb
 {
     public class Program
@@ -8,6 +11,8 @@ namespace E_BookStoreWeb
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
             var app = builder.Build();
 
